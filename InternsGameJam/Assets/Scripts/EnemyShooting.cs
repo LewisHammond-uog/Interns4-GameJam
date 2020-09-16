a3426fd4bf49c3018eb8ee739486ef6d9de225f5
Lewis Hammond <s1800707@glos.ac.uk>
2020-09-16 15:42:17 +0100

    Point Guns towards player

---
+++ InternsGameJam/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : Shooting
+{
+
+    //Random time variace, to simulate them waiting to shoot
+    [SerializeField] private float randomWaitTime = 0f;
+    [SerializeField] private float maxRandomWait = 2f;
+    [SerializeField] private float minRandomWait = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSinceLastShot = 0f;
+        randomWaitTime = GetRandomWaitTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeSinceLastShot > randomWaitTime)
+        {
+            Shoot();
+            randomWaitTime = GetRandomWaitTime();
+            timeSinceLastShot = 0f;
+        }
+    }
+
+    private float GetRandomWaitTime()
+    {
+        return Random.Range((float)minRandomWait, (float)maxRandomWait);
+    }
+}
---
+++ InternsGameJam/Assets/Scripts/ShootingArrayController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingArrayController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    protected float orbitDist = 1.5f;
+
+
+    private void Update()
+    {
+        float angle = GetMouseToPlayerAngle(); 
+        //Rotate sprite that is orbiting around us
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        //Project angle out in a circle by the given distance
+        float xPos = Mathf.Cos(Mathf.Deg2Rad * angle) * orbitDist;
+        float yPos = Mathf.Sin(Mathf.Deg2Rad * angle) * orbitDist;
+
+        //Apply Position
+        transform.position = new Vector3(player.transform.position.x + xPos * 3, player.transform.position.y + yPos * 3, 0);
+
+    }
+
+    protected float GetMouseToPlayerAngle()
+    {
+        //Get the agnle from the mouse to the player
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = player.transform.position.z - Camera.main.transform.position.z;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 mouseLocalPos = mouseWorldPos - player.transform.position;
+        float angle = Mathf.Atan2(mouseLocalPos.y, mouseLocalPos.x) * Mathf.Rad2Deg;
+        return angle;
+    }
+}
