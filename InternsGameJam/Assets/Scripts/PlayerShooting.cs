35482096bf73f60fb4198a020dce0e47da228bfb
Lewis Hammond <s1800707@glos.ac.uk>
2020-09-16 15:03:30 +0100

    Generic Shooting Class
    Created a generic shooting class so shooting is the same between enemies and players

--- InternsGameJam/Assets/Scripts/PlayerShooting.cs
+++ InternsGameJam/Assets/Scripts/PlayerShooting.cs
@@ -2,18 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour
+public class PlayerShooting : Shooting
 {
-    [SerializeField]
-    private Transform[] bulletSpawns; //Points to spawn the bullets at
-
-    [SerializeField]
-    private float shootingCooldown = 0.5f;
-    private float timeSinceLastShot = 0f;
-
-    [SerializeField]
-    private GameObject bulletPrefab;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -38,17 +28,5 @@ public class PlayerShooting : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Shoots from all of the spawn points
-    /// </summary>
-    private void Shoot()
-    {
-        foreach(Transform spawnPoint in bulletSpawns)
-        {
-            GameObject bullet = Instantiate(bulletPrefab);
-            bullet.transform.position = spawnPoint.position;
-            bullet.transform.rotation = spawnPoint.rotation;
-            bullet.GetComponent<Bullet>().Creator = this.gameObject;
-        }
-    }
+
 }
