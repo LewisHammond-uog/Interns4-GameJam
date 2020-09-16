1a3f9a0f89aa22554bd4694cbbc4156aed2f7694
alexpritc <alexpritc@users.noreply.github.com>
2020-09-16 10:26:00 +0100

    Added game manager

---
+++ InternsGameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance { get => instance; set => instance = value; }
+
+    private float gameSpeed;
+
+    public float GameSpeed
+    {
+        get => gameSpeed;
+        set => gameSpeed = value;
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        
+        instance = this;
+    }
+
+    public void TestGameSpeed(int speed)
+    {
+        GameSpeed = speed;
+        
+        Debug.Log("The game speed is now " + GameSpeed);
+    }
+}
