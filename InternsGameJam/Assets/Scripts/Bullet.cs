9e6b645cab67c5d61388475ff0b9ab60ac4dbdb6
Lewis Hammond <s1800707@glos.ac.uk>
2020-09-16 11:21:37 +0100

    Basic Bullet Movement
    - Added Basic bullet movement where it moves in a direction

---
+++ InternsGameJam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private GameObject creator;
+    
+    //Property so creator can only be set if we do not have one already
+    public GameObject Creator
+    {
+        get { return creator; }
+        set { 
+            if (creator == null) {
+                creator = value;    
+            } 
+        }
+    }
+
+    [SerializeField]
+    private float moveSpeed; //Movespeed when time scale is 1
+
+    [SerializeField]
+    private Rigidbody2D bulletRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set our velocity on start
+        if(bulletRB != null)
+        {
+            UpdateVelocityInCurrentDir(moveSpeed);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Sets a new veolcity while keeping the current direction
+    /// </summary>
+    private void UpdateVelocityInCurrentDir(float speed)
+    {
+        bulletRB.velocity = bulletRB.transform.right * speed;
+    }
+
+    /*
+    private void OnEnable()
+    {
+        //Sub to event when the game speed is changed
+        GameManager.GameSpeedChanged += GameManager_GameSpeedChanged;
+    }
+    private void OnDisable()
+    {
+        //Unsubbed when destroyed
+        GameManager.GameSpeedChanged -= GameManager_GameSpeedChanged;
+    }
+
+    private void GameManager_GameSpeedChanged()
+    {
+        throw new System.NotImplementedException();
+    }
+    */
+}
